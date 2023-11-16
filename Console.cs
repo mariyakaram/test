363fac69fc7313d99e47fee859817708a126e064
Mariya <juno2@mail.ru>
2023-11-16 11:51:11 +0300

    новая консоль

---
+++ Console.cs
@@ -0,0 +1,12 @@
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+// = - действие присвоение значения
+// == - проверка на равенство
+// != - проверка на неравенство
+if (m * m == n) // ; ЗАПРЕЩЕНО
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no"); 
---
+++ helloconsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("4 Hello, World!");
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+// = - действие присвоение значения
+// == - проверка на равенство
+// != - проверка на неравенство
+if (m * m == n) // ; ЗАПРЕЩЕНО
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no"); 
