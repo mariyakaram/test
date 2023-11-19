261502a2a08c93807c586c5038b2e8707418dd0e
Mariya <juno2@mail.ru>
2023-11-19 23:55:51 +0700

    исправления

---
+++ helloconsole/hello2/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+*/
+
+/* int numberA = 3; //целое число
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+*/
+
+/* double numberA = 16; //дробное число
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+*/
+
+/* int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+*/
+
+Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+    
+}
+
+
